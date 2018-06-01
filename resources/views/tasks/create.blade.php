0bb6919723f55ddf280c1aca2cdcf5fe66743625
jiro.kirameki <ec2-user@ip-172-31-1-118.us-east-2.compute.internal>
2018-06-01 00:17:47 +0000

    Edit create design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -10,12 +10,12 @@
 
         <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
         </div>
         
         <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
         </div>
     
         {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -11,12 +11,12 @@
 
         <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
         </div>
         
         <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
         </div>
 
         {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}

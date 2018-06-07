5ddab0e62ca295e64fd339aa5518976792c6dc7c
jiro.kirameki <ec2-user@ip-172-31-1-118.us-east-2.compute.internal>
2018-06-07 08:22:11 +0000

    oyobesan shuusei ato

--- database/migrations/2018_06_05_150241_create_tasks_table.php
+++ database/migrations/2018_06_05_150241_create_tasks_table.php
@@ -16,6 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->integer('user_id')->unsigned()->index();
             $table->string('content');
             $table->string('status',10);
         });
